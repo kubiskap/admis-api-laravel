4a32c7b602809ce10b799270e3754e118f8f9e33
Petr Kubíska <kubiskap@gmail.com>
2025-03-19 13:29:31 +0000

    ID parameter in projects route to display individual projects

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::prefix('v1')->group(function () {
         Route::get('me', [AuthController::class, 'me']);
 
         // Projects
-        Route::get('projects/', [ProjectController::class, 'index']); // Get all active projects
+        Route::get('projects/{id?}', [ProjectController::class, 'index']); // Get all active projects
 
         // Users
         Route::apiResource('users', UserController::class); // Full CRUD operations for users
