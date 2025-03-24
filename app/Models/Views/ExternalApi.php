aa53f84b9e77922287fe87b0a79b833f8d9b06aa
Petr Kubíska <kubiskap@gmail.com>
2025-03-24 15:03:12 +0000

    Add more attributes to the model, add MultiLineString as geo_body

--- app/Models/Views/ExternalApi.php
+++ app/Models/Views/ExternalApi.php
@@ -3,6 +3,8 @@
 namespace App\Models\Views;
 
 use Illuminate\Database\Eloquent\Model;
+use MatanYadaev\EloquentSpatial\Objects\MultiLineString;
+use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 
 class ExternalApi extends Model
 {
@@ -40,4 +42,24 @@ class ExternalApi extends Model
      * @var bool
      */
     public $incrementing = false;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'cena'                    => 'float',
+        'gps_n1'                  => 'float',
+        'gps_n2'                  => 'float',
+        'gps_e1'                  => 'float',
+        'gps_e2'                  => 'float',
+        'staniceni_od'           => 'float',
+        'staniceni_do'           => 'float',
+        'priorita_skore'         => 'float',
+        'priorita_korekce'       => 'float',
+        'datum_posledni_zmeny'   => 'datetime',
+        'predani_staveniste'     => 'datetime',
+        'dokonceni_stavby'       => 'datetime',
+        'zaruka_technologicka'   => 'datetime',
+        'zaruka_stavebni'        => 'datetime',
+        'geo_body'               => MultiLineString::class,
+    ];
 }
